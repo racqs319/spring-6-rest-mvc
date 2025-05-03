eb2f6a7e70e87663f9897b037f28fa81c6d160f0
Racquel Cases <racquel.cases@gmail.com>
2025-05-03 14:14:32 +0800

    Implement new method listBeers() in BeerService and add mapping in controller class to list all beers

--- src/main/java/com/casesr/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/casesr/spring6restmvc/controller/BeerController.java
@@ -4,19 +4,28 @@ import com.casesr.spring6restmvc.model.Beer;
 import com.casesr.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @author Racquel.Cases
  */
-@Controller
+@RestController
 @AllArgsConstructor
 @Slf4j
 public class BeerController {
 
     private final BeerService beerService;
+
+    @RequestMapping("/app/v1/beer")
+    public List<Beer> listBeers() {
+
+        return beerService.listBeers();
+
+    }
 
     public Beer getBeerById(UUID id) {
 
--- src/main/java/com/casesr/spring6restmvc/services/BeerService.java
+++ src/main/java/com/casesr/spring6restmvc/services/BeerService.java
@@ -2,12 +2,15 @@ package com.casesr.spring6restmvc.services;
 
 import com.casesr.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @author Racquel.Cases
  */
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 
