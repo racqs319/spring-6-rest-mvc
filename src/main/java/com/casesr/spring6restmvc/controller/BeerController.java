59bda2a2f3ef4b98fda9074f9493148ae2b55849
Racquel Cases <racquel.cases@gmail.com>
2025-05-03 21:00:32 +0800

    Update BeerControllerTest to use WebMvcTest annotation instead of SpringBootTest

--- src/main/java/com/casesr/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/casesr/spring6restmvc/controller/BeerController.java
@@ -2,21 +2,20 @@ package com.casesr.spring6restmvc.controller;
 
 import com.casesr.spring6restmvc.model.Beer;
 import com.casesr.spring6restmvc.services.BeerService;
-import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
-
 /**
  * @author Racquel.Cases
  */
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/beer")
 public class BeerController {
