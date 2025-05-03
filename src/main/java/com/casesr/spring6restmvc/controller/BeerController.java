a6813ce7ddc4ffc8ccb068b2cdac1a8f7cefe40b
Racquel Cases <racquel.cases@gmail.com>
2025-05-03 15:22:24 +0800

    Set header on HTTP Response when creating new Beer object

--- src/main/java/com/casesr/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/casesr/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.casesr.spring6restmvc.model.Beer;
 import com.casesr.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +44,10 @@ public class BeerController {
 
         Beer savedBeer = beerService.saveBeer(beer);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
 
     }
 
