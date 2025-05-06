d23b74c474f7830857367cafc5705029e6fe50a3
Racquel Cases <racquel.cases@gmail.com>
2025-05-06 12:24:15 +0800

    Add unit test for listBeers

--- src/test/java/com/casesr/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/casesr/spring6restmvc/controller/BeerControllerTest.java
@@ -40,4 +40,15 @@ class BeerControllerTest {
         .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
         .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
   }
+
+  @Test
+  void testListBeers() throws Exception {
+    given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+    mockMvc
+        .perform(get("/api/v1/beer").accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.length()", is(3)));
+  }
 }
